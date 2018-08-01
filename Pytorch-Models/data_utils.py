dc0b984ed066cc61ffeba7205d25de69718425d9
Samuel Mensah <samuel@aims.edu.gh>
2018-08-01 12:32:02 -0700

    cleaning code

--- Pytorch-Models/data_utils.py
+++ Pytorch-Models/data_utils.py
@@ -22,11 +22,18 @@ class Data(object):
         self.word_WE    = Variable(torch.from_numpy(self.dataset['word_WE']), requires_grad = True)
         self.pos_WE     = Variable(torch.from_numpy(self.dataset['pos_WE']),  requires_grad = True)
 
-        if shuffle == True:
-            perm = [i for i in range(len(self.dataset[which_data]['arg1']))]
-            np.random.shuffle(perm)
+        type_data = ["train_data", "dev_data", "test_data"]
+        if shuffle is True:
+            if which_data in type_data:
+                perm = [i for i in range(len(self.dataset[which_data]['arg1']))]
+                np.random.shuffle(perm)
+            else:
+                raise KeyError('which_data is either "train_data", "dev_data" or "test_data"')
         else:
-            perm = [i for i in range(len(self.dataset[which_data]['arg1']))]
+            if which_data in type_data:
+                perm = [i for i in range(len(self.dataset[which_data]['arg1']))]
+            else:
+                raise KeyError('which_data is either "train_data", "dev_data" or "test_data"')
 
         if which_data == 'train_data':
             self.arg1 = Variable(torch.from_numpy(self.dataset['train_data']['arg1'][perm]).long(), requires_grad = False)
@@ -59,8 +66,8 @@ class Data(object):
             self.sense    = Variable(torch.from_numpy(sense[perm]).long())
 
         else:
-            raise ValueError('which_data is either "train_data", "dev_data" or "test_data"')
+            raise KeyError('which_data is either "train_data", "dev_data" or "test_data"')
 
 
-#train_data = Data(datafile='temporal_data.pic', which_data='train_data', shuffle=False)
+#train_data = Data(datafile='temporal_data.pic', which_data='train_data', shuffle=True)
 #print(train_data.sense)
